61baa5d20bef0510b27c6bf77e518c64f59b874e
rsobrado <rsobrado@pzfullparty.com>
2013-09-10 14:41:29 -0600

    commit 0001 nos vemos pronto

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('halloween2013App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
